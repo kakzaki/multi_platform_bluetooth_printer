4f488a54feba92e2a000715d07144359ee709bb1
ZAKI MUBAROK <kakzaki@gmail.com>
2023-08-09 17:41:41 +0700

    feat(android): implement all bluetooth feature

--- android/src/main/kotlin/dev/kakzaki/multi_platform_bluetooth_printer/BlePlugin.kt
+++ android/src/main/kotlin/dev/kakzaki/multi_platform_bluetooth_printer/BlePlugin.kt
@@ -11,12 +11,30 @@ class BlePlugin(private val context: Context) {
   // Get BluetoothAdapter instance using BluetoothManager
   private val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
   private val bluetoothAdapter = bluetoothManager.adapter
+  private lateinit var bluetoothGatt : BluetoothGatt
+  private lateinit var serviceUuid : UUID
+  private lateinit var charUuid : UUID
 
   // Callback for GATT client connection state changes
   private val gattCallback = object : BluetoothGattCallback() {
 
+    override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+
+      // Get first service
+      val service = gatt.services[0]
+
+      serviceUuid = service.uuid
+
+      // Get first characteristic
+      val characteristic = service.characteristics[0]
+
+      charUuid = characteristic.uuid
+
+    }
+
     // Handle connected and disconnected state
     override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+      bluetoothGatt = gatt
       when (newState) {
         BluetoothProfile.STATE_CONNECTED -> {
           // Successfully connected
@@ -31,6 +49,7 @@ class BlePlugin(private val context: Context) {
 
     // Handle characteristic write result
     override fun onCharacteristicWrite(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
+      bluetoothGatt = gatt
       when (status) {
         BluetoothGatt.GATT_SUCCESS -> {
           // Characteristic was written successfully
@@ -53,30 +72,31 @@ class BlePlugin(private val context: Context) {
   }
 
   // Connect to device
-  fun connect(device: BluetoothDevice) {
+
+  fun connect(address: String) {
+    val device = bluetoothAdapter.getRemoteDevice(address)
     device.connectGatt(context, false, gattCallback)
   }
 
   // Disconnect GATT client
-  fun disconnect(gatt: BluetoothGatt) {
-    gatt.disconnect()
+  fun disconnect() {
+    bluetoothGatt.disconnect()
   }
 
   // Write data to characteristic
-  fun writeData(gatt: BluetoothGatt, serviceUuid: UUID, charUuid: UUID, data: ByteArray) {
-
-    val service = gatt.getService(serviceUuid)
+  fun writeData(data: ByteArray) {
+    val service = bluetoothGatt.getService(serviceUuid)
     val characteristic = service.getCharacteristic(charUuid)
 
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
       // Set the value of the characteristic to the data
-      gatt.writeCharacteristic(characteristic,data,BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
+      bluetoothGatt.writeCharacteristic(characteristic,data,BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
     } else {
 
       characteristic.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
       // Set the value of the characteristic to the data
       characteristic.value=data
-      gatt.writeCharacteristic(characteristic)
+      bluetoothGatt.writeCharacteristic(characteristic)
     }
 
   }
--- android/src/main/kotlin/dev/kakzaki/multi_platform_bluetooth_printer/MultiPlatformBluetoothPrinterPlugin.kt
+++ android/src/main/kotlin/dev/kakzaki/multi_platform_bluetooth_printer/MultiPlatformBluetoothPrinterPlugin.kt
@@ -1,7 +1,11 @@
 package dev.kakzaki.multi_platform_bluetooth_printer
 
-import androidx.annotation.NonNull
-
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -15,21 +19,40 @@ class MultiPlatformBluetoothPrinterPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var ble : BlePlugin
+  private lateinit var bluetoothAdapter: BluetoothAdapter
+  private lateinit var mContext: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "multi_platform_bluetooth_printer")
     channel.setMethodCallHandler(this)
+    mContext = flutterPluginBinding.applicationContext;
+    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    ble = BlePlugin(mContext)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when(call.method){
+      "startScanning" -> ble.startScanning(scanCallback)
+      "stopScanning" -> ble.stopScanning(scanCallback)
+      "connect" -> {
+        ble.connect("Address")
+      }
+      "disconnect" -> ble.disconnect()
+      "writeData" -> ble.writeData()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
+  private val scanCallback = object : ScanCallback() {
+
+    override fun onScanResult(callbackType: Int, result: ScanResult) {
+      var deviceId = result.device.address;
+      Log.d("ScanResult", "$result")
+    }
+  }
+
 }
